dbd9dbd3e245d7ea4d75761307fb01ecc23c999a
Marian <maazurin@gmail.com>
2019-08-03 20:08:47 -0700

    initial code

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+//read data
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM burgers_db.burgers", function (err, res) {
+        if (err) throw err;
+
+        console.log(res);
+        connection.end();
+    });
+}
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken Bacon Burger", TRUE), 
+("Double Beef Meaty Patties Burger", False),
+("Bacon Ranch Double Beef Burger", True);
